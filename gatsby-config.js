96bd0c7a050c500c3480b3c7e5ad3b9b1e3cf23a
Julian <juliantellezmendez@gmail.com>
2021-10-20 03:22:13 +0100

    chore(blog): hello world

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,60 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "personal-website-gatsby",
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "TODO-CHANGE-ME",
-      },
+    siteMetadata: {
+        siteUrl: "https://juliantellez.com",
+        title: "Devscapades",
+        description: "Personal blog and corner of the web",
+        author: "Julian Tellez",
+        social: {
+            twitter: '@_juliantellez',
+        },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+        {
+            resolve: "gatsby-transformer-remark",
+            options: {
+                plugins: [{
+                    resolve: 'gatsby-remark-external-links',
+                    options: {
+                        target: '_blank',
+                    },
+                }, ]
+            }
+        },
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "TODO-CHANGE-ME",
+            },
+        },
+
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+    ],
 };
